123740ebb2e87a80cb93fc1eb28c22ecae6b6000
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-21 10:58:06 +0200

    posts

--- src/components/posts/PostsList.tsx
+++ src/components/posts/PostsList.tsx
@@ -1,25 +1,45 @@
 //
 
 import { useEffect, useState } from 'react';
+import { PostObj } from '../../types/types';
 
 const postUrl = 'https://jsonplaceholder.typicode.com/posts';
 
 export default function PostsList() {
-  const [postsArr, setPostsArr] = useState([]);
-
+  const [postsArr, setPostsArr] = useState<PostObj[] | []>([]);
+  console.log('postsArr ===', postsArr);
   useEffect(() => {
     getPosts();
   }, []);
 
   function getPosts() {
     // susiusti fetch
+    fetch(postUrl)
+      .then((resp) => resp.json())
+      .then((ats: PostObj[]) => {
+        setPostsArr(ats.slice(0, 15));
+        // console.log('ats ===', JSON.stringify(ats[0]));
+      })
+      .catch((error: Error) => {
+        console.warn('ivyko klaida:', error.message);
+      });
   }
 
   return (
     <div>
       <h2>Posts</h2>
-      <ul className='list-group'>
-        <li className='list-group-item'>post 1</li>
+      <ul className='row unlisted'>
+        {postsArr.map((pObj) => (
+          <li className='col-md-4'>
+            <div className='card text-bg-light mb-3'>
+              <div className='card-header'>Post id: {pObj.id}</div>
+              <div className='card-body'>
+                <h5 className='card-title'>{pObj.title}</h5>
+                <p className='card-text'>{pObj.body}</p>
+              </div>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,10 @@ export type UserObjType = {
   email: string;
   password: string;
 };
+
+export type PostObj = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
