f882f52e9ba1072da2dcda3fa07b00610724879d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-21 08:48:38 +0200

    Add state and event handlers for email and password inputs in Login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,63 @@
+import { useState } from 'react';
+
 //
 export default function Login() {
+  const [emailVal, setEmailVal] = useState('');
+  const [passVal, setPassVal] = useState('');
+
+  function enterEmailHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log('event.target.value ===', event.target.value);
+    setEmailVal(event.target.value);
+  }
+
+  const ennterPasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassVal(e.target.value);
+  };
+
+  const submitFormHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    // sustabdyti forma nuo psl perkrovimo
+    event.preventDefault();
+    // atspausdinti user objekta {email: '', password: ''}
+    const userObj = {
+      email: emailVal,
+      password: passVal,
+    };
+    console.log('userObj ===', userObj);
+  };
+
   return (
     <div>
       <h2>Login</h2>
-      <form noValidate>
+      <div>
+        <p>Email: {emailVal}</p>
+        <p>Pass: {passVal}</p>
+      </div>
+      <form onSubmit={submitFormHandler} noValidate>
         {/* email input */}
         <div className='mb-3'>
           <label htmlFor='email' className='form-label'>
             Email address
           </label>
-          <input type='email' className='form-control' id='email' aria-describedby='emailHelp' />
+          <input
+            value={emailVal}
+            onChange={enterEmailHandler}
+            type='email'
+            className='form-control'
+            id='email'
+          />
         </div>
         {/* pass input */}
         <div className='mb-3'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-control' id='password' />
+          <input
+            value={passVal}
+            onChange={ennterPasswordHandler}
+            type='password'
+            className='form-control'
+            id='password'
+          />
         </div>
 
         <button type='submit' className='btn btn-primary'>
