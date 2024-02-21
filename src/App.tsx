a7d4df25fdf6a1efd09ccb0552ffd140ed3050a9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-21 08:54:42 +0200

    uzd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 // import React from 'react';
+import { useState } from 'react';
 import Login from './components/Login';
 
 export default function App() {
+  const [userObj, setUserObj] = useState();
+
+  function getUser() {
+    // gauti sita console log be klaidu
+    console.log('getUser ivyko');
+    // pamiti useri is Login formos ir irasyti ji i userObj
+
+    // kai turim userObj is login, tai neberodom formos rodom kortele
+  }
+
   return (
     <div className='container'>
       <h1 className='display-2'>App</h1>
-      <Login />
+      <Login onLogin={getUser} />
+      <div className='card bg-dark-subtle'>
+        <div className='card-body'>
+          <h2>Sveiki sugryze</h2>
+        </div>
+      </div>
     </div>
   );
 }
