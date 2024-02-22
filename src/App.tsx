17dd0d0e60f9e673bc0b67d6ece3254ec2cd8adb
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-22 08:25:32 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 // import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage';
+import PostsPage from './pages/PostsPage';
+import AboutPage from './pages/AboutPage';
 
 export default function App() {
   return (
     <div className=''>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/posts' element={<PostsPage />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
