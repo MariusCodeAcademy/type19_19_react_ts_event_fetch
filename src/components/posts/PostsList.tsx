7864e4a573749c6c4cb850d1f8c2fdf3025dbe87
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-22 10:41:17 +0200

    empty filter

--- src/components/posts/PostsList.tsx
+++ src/components/posts/PostsList.tsx
@@ -14,10 +14,6 @@ export default function PostsList() {
   // [{id: 1, tag: 'american'}, {id: 2, tag: 'history'}]
   console.log('postsArr ===', postsArr);
   useEffect(() => {
-    // (async () => {
-    //   const posts = await getPosts(postUrl);
-    //   setPostsArr(posts);
-    // })();
     // Bendrinis tipas (generics)
     getApiData<DummyResponseType>(dummyPostUrl).then((data) => {
       if (!data) return;
@@ -28,6 +24,22 @@ export default function PostsList() {
 
   return (
     <div>
+      <div className='card'>
+        <div className='card-body'>
+          <h3>Filters</h3>
+          <div>
+            <label htmlFor='limit'>Limit</label>
+            <select className='form-select' id='limit' aria-label='Default select example'>
+              <option selected value='all'>
+                All
+              </option>
+              <option value='5'>5</option>
+              <option value='10'>10</option>
+              <option value='15'>15</option>
+            </select>
+          </div>
+        </div>
+      </div>
       <ul className='row unlisted mt-4'>
         {postsArr.map((pObj) => (
           <li key={pObj.id} className='col-md-4 col-sm-6'>
